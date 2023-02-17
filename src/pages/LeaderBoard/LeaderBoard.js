0fe1c1d0ce6128b0f4714bb0e17f44f46c3c0ded
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-02-17 21:39:51 +0530

    leaders api enabled

--- src/pages/LeaderBoard/LeaderBoard.js
+++ src/pages/LeaderBoard/LeaderBoard.js
@@ -1,21 +1,21 @@
-import React, { } from 'react'
-// import { LeaderCard } from '../../components'
+import React, { useState,useEffect } from 'react'
+import { LeaderCard } from '../../components'
 import './LeaderBoard.css'
-// import axios from 'axios'
-// import { caBaseUrl } from '../../utils/urls'
+import axios from 'axios'
+import { caBaseUrl } from '../../utils/urls'
 import lines from './../../assets/heading_lines.svg'
 import line_rt from './../../assets/heading_lines_rt.svg'
 function LeaderBoard() {
-  // const[leaders,setLeaders]=useState([])
-  // useEffect(()=>{
-  //   axios.get(`${caBaseUrl}/ambassadorLeaderboard`).then((response)=>{
-  //     setLeaders(response.data);
+  const[leaders,setLeaders]=useState([])
+  useEffect(()=>{
+    axios.get(`${caBaseUrl}/ambassadorLeaderboard`).then((response)=>{
+      setLeaders(response.data);
 
-  // },(error)=>{
-  //     console.log(error)
-  // })
+  },(error)=>{
+      console.log(error)
+  })
 
-  // },[])
+  },[])
   return (
     <div className="leaderBoard">
         <div className='contact_title'>
@@ -24,12 +24,11 @@ function LeaderBoard() {
             <img src={line_rt} alt=' ' className='contact_lines'/>            
         </div>
         <div className='leaderBoard__component'>
-          {/* {leaders?.map((leader,index)=>{
+          {leaders?.map((leader,index)=>{
             return(
               <LeaderCard rank={index+1} name={leader.name} score={leader.points} skeleton={false} avatar={leader.image}/>
             )
-          })} */}
-             <h3>Coming Soon...</h3>
+          })}
         </div>
     </div>
   )
